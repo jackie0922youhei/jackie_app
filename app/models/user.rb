779e1146b7fcf3c140e077377e8b4c19c08d076d
jackie0922youhei <jackie0922youhei@gmail.com>
2021-02-15 18:59:15 +0900

    mailer test

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,7 +45,7 @@ class User < ApplicationRecord
   
   # 有効化トークンとダイジェストを作成および代入する
   def create_activation_digest
-    self.activation_token  = User.new_token
+    self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
   end
 end
